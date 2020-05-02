e4d8d934667d12390e8a07200e1456506d7518c3
biezz21 <biezz0210@gmail.com>
2020-05-02 23:35:27 +0900

    add view -ref dictionary

--- UploadApp/views.py
+++ UploadApp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import V1Model, V2Model
 from django.core.files.storage import FileSystemStorage
+import pandas as pd
+import numpy as np
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+import os
+from django.conf import settings
 
 # Create your views here.
 
@@ -11,6 +18,34 @@ def UploadView(request):
         filename = fs.save(vcf_file.name, vcf_file)
         uploaded_file_url = fs.url(filename)
         refversion = request.POST.get('Refversion')
+
+        # V4
+        ref_file    = open(os.path.join(settings.MEDIA_ROOT, 'glyma.Wm82.gnm4.4PTR.genome_main.fna'))
+        f           = ref_file.read()
+        ref         = f.split('>')
+
+        dicfa_v4    = {}
+        for x in ref[1:]:
+            title           = x.split('\n')[0].split()[0].split('.')[-1]
+            seq             = ''.join(x.split('\n')[1:])
+            dicfa_v4[title] = seq
+
+        # Select Ref Version
+        if refversion == 'Glycine max accession Williams genome assembly v1.0':
+            ref_file    = open(os.path.join(settings.MEDIA_ROOT, 'glyma.Wm82.gnm1.FCtY.genome_main.fna'))
+        elif refversion == 'Glycine max accession Williams 82 genome assembly v2.0':
+            ref_file    = open(os.path.join(settings.MEDIA_ROOT, 'glyma.Wm82.gnm2.DTC4.genome_main.fna'))
+            
+        f           = ref_file.read()
+        ref         = f.split('>')
+
+        dicfa_pre   = {}
+        for x in ref[1:]:
+            title            = x.split('\n')[0].split()[0].split('.')[-1]
+            seq              = ''.join(x.split('\n')[1:])
+            dicfa_pre[title] = seq
+
+            
 
         obj_output = {
             'uploaded_file_url': uploaded_file_url,
