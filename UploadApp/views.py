fea3b68f23fc0294a009896f16e7c3020ccfe220
biezz21 <biezz0210@gmail.com>
2020-05-03 23:04:07 +0900

    add vcf read function

--- UploadApp/views.py
+++ UploadApp/views.py
@@ -9,6 +9,7 @@ from Bio import SeqIO
 import os
 from django.conf import settings
 
+
 # Create your views here.
 
 def UploadView(request):
@@ -20,15 +21,15 @@ def UploadView(request):
         refversion = request.POST.get('Refversion')
 
         # V4
-        ref_file    = open(os.path.join(settings.MEDIA_ROOT, 'glyma.Wm82.gnm4.4PTR.genome_main.fna'))
-        f           = ref_file.read()
-        ref         = f.split('>')
+        # ref_file    = open(os.path.join(settings.MEDIA_ROOT, 'glyma.Wm82.gnm4.4PTR.genome_main.fna'))
+        # f           = ref_file.read()
+        # ref         = f.split('>')
 
-        dicfa_v4    = {}
-        for x in ref[1:]:
-            title           = x.split('\n')[0].split()[0].split('.')[-1]
-            seq             = ''.join(x.split('\n')[1:])
-            dicfa_v4[title] = seq
+        # dicfa_v4    = {}
+        # for x in ref[1:]:
+        #     title           = x.split('\n')[0].split()[0].split('.')[-1]
+        #     seq             = ''.join(x.split('\n')[1:])
+        #     dicfa_v4[title] = seq
 
         # Select Ref Version
         if refversion == 'Glycine max accession Williams genome assembly v1.0':
@@ -45,8 +46,9 @@ def UploadView(request):
             seq              = ''.join(x.split('\n')[1:])
             dicfa_pre[title] = seq
 
+        vcf_mat = pd.read_csv('%s'%os.path.join(settings.MEDIA_ROOT, filename), compression='gzip',
+                comment='#', sep='\t', header=None)
             
-
         obj_output = {
             'uploaded_file_url': uploaded_file_url,
             'refversion': refversion,
